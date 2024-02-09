ac71ac74727f8e11ffea46cc685676dc544a4e8c
Andrey Dorofeev <116450431+TurnManEDITION@users.noreply.github.com>
2024-02-09 19:39:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,349 @@
+var delay = 100;
+var text = "Home";
+var elem = document.getElementById("home");
+var print_text =
+function(text, elem, delay) {
+    if(text.length > 0) {
+        elem.innerHTML += text[0];
+        setTimeout(
+            function() {
+                print_text(text.slice(1), elem, delay); 
+            }, delay
+        );
+    }
+}
+print_text(text, elem, delay);
+console.log("Home");
+//--------------------------------------------------------------------------
+var delay = 100;
+var text = "Contacts";
+var elem = document.getElementById("contacts");
+var print_text = 
+function(text, elem, delay) {
+    if(text.length > 0) {
+        elem.innerHTML += text[0];
+        setTimeout(
+            function() {
+                print_text(text.slice(1), elem, delay); 
+            }, delay
+        );
+    }
+}
+print_text(text, elem, delay);
+console.log("Contacts");
+//--------------------------------------------------------------------------
+var delay = 100;
+var text = "Projects";
+var elem = document.getElementById("projects");
+var print_text = 
+function(text, elem, delay) {
+    if(text.length > 0) {
+        elem.innerHTML += text[0];
+        setTimeout(
+            function() {
+                print_text(text.slice(1), elem, delay); 
+            }, delay
+        );
+    }
+}
+print_text(text, elem, delay);
+console.log("projects");
+//--------------------------------------------------------------------------
+but1.onclick = 
+function() {
+    var delay = 100;
+    var text = "1";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("1");
+}
+but2.onclick = 
+function() {
+    var delay = 100;
+    var text = "2";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("2");
+}
+but3.onclick = 
+function() {
+    var delay = 100;
+    var text = "3";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("3");
+}
+but4.onclick = 
+function() {
+    var delay = 100;
+    var text = "4";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("4");
+}
+but5.onclick = 
+function() {
+    var delay = 100;
+    var text = "5";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("5");
+}
+but6.onclick = 
+function() {
+    var delay = 100;
+    var text = "6";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("6");
+}
+but7.onclick = 
+function() {
+    var delay = 100;
+    var text = "7";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("7");
+}
+but8.onclick = 
+function() {
+    var delay = 100;
+    var text = "8";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("8");
+}
+but9.onclick = 
+function() {
+    var delay = 100;
+    var text = "9";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("9");
+}
+//---------------------------------------------------------------------------------
+multply.onclick = 
+function() {
+    var delay = 100;
+    var text = "*";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("*");
+}
+division.onclick = 
+function() {
+    var delay = 100;
+    var text = "/";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("/");
+}
+plus.onclick = 
+function() {
+    var delay = 100;
+    var text = "+";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("+");
+}
+minus.onclick = 
+function() {
+    var delay = 100;
+    var text = "-";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("-");
+}
+degree.onclick = 
+function() {
+    var delay = 100;
+    var text = "^";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("^");
+}
+exactle.onclick = 
+function() {
+    var delay = 100;
+    var text = "=";
+    var elem = document.getElementById("number");
+    var print_text = 
+    function(text, elem, delay) {
+        if(text.length > 0) {
+            elem.innerHTML += text[0];
+            setTimeout(
+                function() {
+                    print_text(text.slice(1), elem, delay); 
+                }, delay
+            );
+        }
+    }
+    print_text(text, elem, delay);
+    console.log("=");
+}
+
+
+
+
+
+
+del.onclick =
+function() {
+    var elem = document.getElementById("number").textContent;
+    console.log(elem.slice(0, -1).toString());
+    number.textContent = elem.slice(0, -1).toString();
+}
